d65e88f8fea9b172698a1d9da6180236b1ba5ae1
ishigetani <k.subaru1230@gmail.com>
2014-12-27 16:01:11 +0900

    notify import. http://notifyjs.com/

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -11,20 +11,26 @@ angular.module('mainCtrl', []).controller('mainController', function($scope, $ht
     Comment.get().success(function(data) {
         $scope.comments = data.response;
         $scope.loading = false;
+    }).error(function(data) {
+        $.notify("Error.", 'error');
     });
 
     $scope.submitComment = function() {
         Comment.save($scope.commentData).success(function(data) {
             $scope.comments.unshift(data.response);
             $scope.commentData.text = '';
+            $.notify("Complete.", 'success');
         }).error(function(data) {
-            console.log(data);
+            $.notify("Save Error.", 'error');
         });
     };
 
     $scope.deleteComment = function(index) {
         Comment.deleteId($scope.comments[index].Comment.id).success(function(data){
             $scope.comments.splice(index, 1);
+            $.notify("Complete.", 'success');
+        }).error(function(data) {
+            $.notify("Delete Error.", 'error');
         })
     }
 });
