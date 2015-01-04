2801e5681e89b81ad4f0c0a011a21e334aee2fce
ishigetani <k.subaru1230@gmail.com>
2015-01-04 16:05:49 +0900

    edit updater.

--- js/app.js
+++ js/app.js
@@ -2,4 +2,4 @@
  * Created by ishigetani on 2014/12/26.
  */
 
-var commentApp = angular.module('commentApp', ['mainCtrl', 'commentService', 'constantService', 'angular-loading-bar', 'ngAnimate', 'ngStorage']);
+var commentApp = angular.module('commentApp', ['mainCtrl', 'commentService', 'constantService', 'angular-loading-bar', 'ngAnimate', 'ngStorage', 'xeditable']);
